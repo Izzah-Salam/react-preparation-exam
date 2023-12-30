4a7ce6fbdeea51e51c49e16cc385684bb1b18f12
Izzah Salam <izzahsalam00@gmail.com>
2023-12-30 12:21:35 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+This template contain initial setup for starting a react app.
+
+Tailwind is install
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+	return <div className="text-3xl text-red-400 font-medium">App</div>;
+}
+export default App;
